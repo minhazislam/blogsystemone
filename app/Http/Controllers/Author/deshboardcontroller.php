1c9b0d25870ac90fdc528ebd015d659e66891ef8
mijone <mijone880@gmail.com>
2019-09-12 20:05:34 +0600

    projectfile

---
+++ app/Http/Controllers/Author/deshboardcontroller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Author;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class deshboardcontroller extends Controller
+{
+    return view('author.deshboard');
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['as'=>'admin.','prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth','admin']], function (){
+		Route::get('/deshboard','deshboardcontroller@index')->name('deshboard');
+
+	
+});
+
+Route::group(['as'=>'author.','prefix'=>'author','namespace'=>'Author','middleware'=>['auth','author']], function (){
+    Route::get('/deshboard','deshboardcontroller@index')->name('deshboard');
+	
+});
